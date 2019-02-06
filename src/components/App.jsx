62ef95d85eca1c687afd0fc0dae23d2fda0c6904
Chloe Verity <chloeverity@hotmail.co.uk>
2019-02-06 14:18:17 +0000

    installing react-router to configure routes, adding in one route that points to voting component, creating app.jsx as root component that renders its child components to be given in as the children props

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {List} from 'immutable';
+
+const pair = List.of("Trainspotting", "28 Days Later");
+
+var createReactClass = require('create-react-class');
+
+const App = createReactClass({
+  render: function() {
+    return React.cloneElement(this.props.children, {pair: pair});
+  }
+});
