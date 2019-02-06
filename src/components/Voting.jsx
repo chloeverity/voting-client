96f08b964976c3679e9ebc0689183a88a5066153
Chloe Verity <chloeverity@hotmail.co.uk>
2019-02-06 11:37:33 +0000

    hardcoding in a hasVoted prop (keeping components pure) as don't want user to vote twice on same pair; adding disable message, and hasVotedFor method to decide whether to render the label or not

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -6,10 +6,17 @@ const Voting = createReactClass({
   getPair: function() {
     return this.props.pair || [];
   },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor: function(entry) {
+    return this.props.hasVoted === entry;
+  },
   render: function() {
     return <div className="voting">
       {this.getPair().map(entry =>
         <button key={entry}
+                disabled={this.isDisabled()}
                 onClick={() => this.props.vote(entry)}>
           <h1>{entry}</h1>
         </button>
